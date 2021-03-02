a9f5e655c6db1984c634c15e47c38e28bc1eeb96
Elkin Sierra <firewalledsc86@gmail.com>
2021-03-02 16:20:23 -0500

    get current count

--- server/public/js/simpleCount.js
+++ server/public/js/simpleCount.js
@@ -1,6 +1,14 @@
+// fetch APi to get current counts
+document.addEventListener("DOMContentLoaded", getCount);
 
 // hit API to add to count
 document.querySelector(".simpleCount-btn").addEventListener("click", addCount);
+
+function getCount() {
+  axios.get('https://api.countapi.xyz/get/1ccb732e-b55a-4404-ad3f-0f99c02fe44e-Test/purpleCount').then(responce => {
+        document.querySelector(".simpleCount").innerHTML = responce.data.value;
+    });
+}
 
 // hits api to increase current count and injects result to HTML
 function addCount() {
